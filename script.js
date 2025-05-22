2c6a64c6ce962a60d40a32279fe1f20d0f04406a
Haydenbobo <boboxode@gmail.com>
2025-05-22 09:02:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// You can optionally use this for client-side confirmation if you are not redirecting after SubmitForm
+document.querySelector(".vertical-form").addEventListener("submit", function (e) {
+  alert("Thank you for reaching out to BOBO ENGINEERING!");
+});
